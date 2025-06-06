3147b15549ee1238f3073b3aee2a783507a97684
David Papp <pappdav@gmail.com>
2025-06-06 18:54:37 +0200

    Update Arxignis formula to version 0.0.2 with new download URLs and SHA256 checksums for macOS and Linux platforms.

--- Formula/arxignis.rb
+++ Formula/arxignis.rb
@@ -4,27 +4,31 @@
 class Arxignis < Formula
   desc "Headless AppSec Security platform"
   homepage "https://arxignis.com"
-  version "0.0.1"
+  version "0.0.2"
   license "Apache-2.0"
-  url "https://github.com/arxignis/cli/releases/download/v0.0.1/ax.darwin-arm64.tar.gz"
-  sha256 "d412126e7e15d911589e7e543a82c207b10b2fd9f6c22f28b7c0dfd182cde68c"
+  url "https://github.com/arxignis/cli/releases/download/v0.0.2/ax.darwin-arm64.tar.gz"
+  sha256 "d395aa4fbb28f7ad79a889e8447bb6bde36c499879220d75be67a1adb2e1739e"
   version_scheme 1
 
   on_macos do
     on_arm do
-      url "https://github.com/arxignis/cli/releases/download/v0.0.1/ax.darwin-arm64.tar.gz"
-      sha256 "d412126e7e15d911589e7e543a82c207b10b2fd9f6c22f28b7c0dfd182cde68c"
+      url "https://github.com/arxignis/cli/releases/download/v0.0.2/ax.darwin-arm64.tar.gz"
+      sha256 "d395aa4fbb28f7ad79a889e8447bb6bde36c499879220d75be67a1adb2e1739e"
+    end
+    on_intel do
+      url "https://github.com/arxignis/cli/releases/download/v0.0.2/ax.darwin-amd64.tar.gz"
+      sha256 "30ea5d214b14c05ab7bef1b923a671c7d6d7479c88df1f55f06ee625c72015aa"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/arxignis/cli/releases/download/v0.0.1/ax.linux-amd64.tar.gz"
-      sha256 "c4ef698c4d82d0893b1d004df1205b5884ed32b80d9f7fe9482aa33025bf330f"
+      url "https://github.com/arxignis/cli/releases/download/v0.0.2/ax.linux-amd64.tar.gz"
+      sha256 "5c53f14dca5403587aa26c1c563523183a164906a4cf7774d3ba87123ae1c8ce"
     end
     on_arm do
-      url "https://github.com/arxignis/cli/releases/download/v0.0.1/ax.linux-arm64.tar.gz"
-      sha256 "cf35751af4515254cd398197817a8c5c635b0a7d70946f7f6263cd15d8bd3ba8"
+      url "https://github.com/arxignis/cli/releases/download/v0.0.2/ax.linux-arm64.tar.gz"
+      sha256 "dc6d605a102f9dc3e480adfeb525cce708f389b9cff527e5bef0c582b16c9a7c"
     end
   end
 
@@ -32,6 +36,8 @@ class Arxignis < Formula
     libexec.install Dir["*"]
     if OS.mac? && Hardware::CPU.arm?
       bin.install_symlink libexec/"ax-darwin-arm64" => "ax"
+    elsif OS.mac? && Hardware::CPU.intel?
+      bin.install_symlink libexec/"ax-darwin-amd64" => "ax"
     elsif OS.linux? && Hardware::CPU.intel?
       bin.install_symlink libexec/"ax-linux-amd64" => "ax"
     elsif OS.linux? && Hardware::CPU.arm?
