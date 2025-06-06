3d521eb208d15b3a8b764ff93aae05d436209cd6
David Papp <pappdav@gmail.com>
2025-06-06 16:27:33 +0200

    First release

---
+++ Formula/arxignis.rb
@@ -0,0 +1,44 @@
+class Arxignis < Formula
+  desc "Headless AppSec Security platform"
+  homepage "https://arxignis.com"
+  version "0.0.1"
+  version_scheme 1
+
+  on_macos do
+    on_arm do
+      url "https://github.com/arxignis/cli/releases/download/v0.0.1/ax.darwin-arm64.tar.gz"
+      sha256 "d412126e7e15d911589e7e543a82c207b10b2fd9f6c22f28b7c0dfd182cde68c"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/arxignis/cli/releases/download/v0.0.1/ax.linux-amd64.tar.gz"
+      sha256 "c4ef698c4d82d0893b1d004df1205b5884ed32b80d9f7fe9482aa33025bf330f"
+    end
+    on_arm do
+      url "https://github.com/arxignis/cli/releases/download/v0.0.1/ax.linux-arm64.tar.gz"
+      sha256 "cf35751af4515254cd398197817a8c5c635b0a7d70946f7f6263cd15d8bd3ba8"
+    end
+  end
+
+  def install
+    libexec.install Dir["*"]
+    if OS.mac? && Hardware::CPU.arm?
+      bin.install_symlink libexec/"ax-darwin-arm64" => "ax"
+    elsif OS.linux? && Hardware::CPU.intel?
+      bin.install_symlink libexec/"ax-linux-amd64" => "ax"
+    elsif OS.linux? && Hardware::CPU.arm?
+      bin.install_symlink libexec/"ax-linux-arm64" => "ax"
+    end
+  end
+
+  def caveats; <<~EOS
+
+  EOS
+  end
+
+  test do
+    system bin/"ax", "version"
+  end
+end
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# homebrew-brew
+# Homebrew Tap for Arxignis
+
+## Installation
+
+You can install Arxignis CLI using Homebrew:
+
+```bash
+# Add the tap
+brew tap arxignis/brew
+
+# Install the formula
+brew install arxignis
+```
+
+Or install directly:
+```bash
+brew install arxignis/brew/arxignis
+```
+
+## Documentation
+
+For more information, visit [arxignis.com](https://arxignis.com)
